94fba97cc4f5c3202a14690e55a958e789ba4098
Haniyeh Nasseri <haniyeh.nasseri99@gmail.com>
2021-06-10 19:56:19 +0430

    infrom arrive bluetooth

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -32,31 +32,26 @@ void loop() {
     vSerial.println("start capture ...");
     comSerial.println("startcapture");
     vSerial.println("capture done ...");
+    Serial.println("someone is here !\n" );
+    delay(4000);
+    if(Serial.available() > 0){
+      String newMsg = Serial.readString();
+      vSerial.println(newMsg); 
+    }
   }
 
-//  if (comSerial.available() > 0){ // Check if there is data coming from camera
-//    pic = comSerial.readString(); // Read the message as String
-//  }
-
-  // To read message received from other Bluetooth Device
+  else{
+      // To read message received from other Bluetooth Device
   if (Serial.available() > 0){ // Check if there is data coming
     msg = Serial.readString();
     // Read the message as String
     vSerial.println("Android Command: " + msg);
   }
-
-  // Control LED in Arduino board
-  if (msg == "haniyeh"){
-    digitalWrite(ledPin, HIGH); // Turn on LED
-    vSerial.println("LED is turned on\n"); // Then send status message to Android
-    msg = ""; // reset command
-  } else {
-    if (msg == "samin"){
-      digitalWrite(ledPin, LOW); // Turn off LED
-      vSerial.println("LED is turned off\n"); // Then send status message to Android
-      msg = ""; // reset command
-    }
   }
 
+
+
+
   while(digitalRead(capture) == HIGH);
+  delay(1000);
 }
